090381b380c109433bacd8d81d2373cba36a1010
Dragan Milic <dragan@netice9.com>
2017-09-03 21:12:21 +0100

    added listener convenience method, global add screen

--- kickback.go
+++ kickback.go
@@ -1,10 +1,10 @@
 package kickback
 
 import (
-	"log"
-
 	reactor "github.com/draganm/go-reactor"
 	"github.com/draganm/immersadb"
+	"github.com/draganm/immersadb/dbpath"
+	"github.com/draganm/immersadb/modifier"
 	"github.com/urfave/negroni"
 )
 
@@ -14,18 +14,41 @@ type Context struct {
 	MountFunc     func()
 	UserEventFunc func(*reactor.UserEvent)
 	UnmountFunc   func()
+	listeners     []*listener
+}
+
+type listener struct {
+	db   *immersadb.ImmersaDB
+	path dbpath.Path
+	f    func(er modifier.EntityReader)
+}
+
+func (l *listener) unsubscribe() {
+	l.db.RemoveListener(l.path, l)
+}
+
+func (l *listener) OnChange(r modifier.EntityReader) {
+	l.f(r)
+}
+
+func (c *Context) Listen(dbpth dbpath.Path, f func(er modifier.EntityReader)) {
+	l := &listener{db: c.DB, path: dbpth, f: f}
+	c.listeners = append(c.listeners, l)
+	c.DB.AddListener(dbpth, l)
 }
 
 func (c *Context) Mount() {
 	if c.MountFunc != nil {
 		c.MountFunc()
-		log.Println("c")
 	}
 }
 
 func (c *Context) Unmount() {
 	if c.UnmountFunc != nil {
 		c.UnmountFunc()
+	}
+	for _, l := range c.listeners {
+		l.unsubscribe()
 	}
 }
 
@@ -35,19 +58,23 @@ func (c *Context) OnUserEvent(evt *reactor.UserEvent) {
 	}
 }
 
-type Kickback struct {
+type kickback struct {
 	reactor *reactor.Reactor
 	db      *immersadb.ImmersaDB
 }
 
-func New(db *immersadb.ImmersaDB, handlers ...negroni.Handler) *Kickback {
-	return &Kickback{
+func Run(addr string, db *immersadb.ImmersaDB, handlers []negroni.Handler) {
+	k := &kickback{
 		reactor: reactor.New(handlers...),
 		db:      db,
 	}
+	for _, s := range Screens {
+		k.AddScreen(s.Path, s.Factory)
+	}
+	k.reactor.Serve(addr)
 }
 
-func (k *Kickback) AddScreen(path string, s func(*Context)) {
+func (k *kickback) AddScreen(path string, s func(*Context)) {
 	k.reactor.AddScreen(path, func(ctx reactor.ScreenContext) reactor.Screen {
 		sctx := &Context{
 			ScreenContext: ctx,
@@ -58,6 +85,13 @@ func (k *Kickback) AddScreen(path string, s func(*Context)) {
 	})
 }
 
-func (k *Kickback) Serve(addr string) {
-	k.reactor.Serve(addr)
+type AddedScreen struct {
+	Path    string
+	Factory func(*Context)
+}
+
+var Screens = []*AddedScreen{}
+
+func AddScreen(path string, s func(*Context)) {
+	Screens = append(Screens, &AddedScreen{Path: path, Factory: s})
 }
